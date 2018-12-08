00bd507d9e18f9285e85b7e875f2c63359f5604c
Cadman <jcadiente505@gmail.com>
2018-12-08 15:31:17 -0500

    practicing creating components and styling

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+// adding a decorator is a way to enhance Angular classes
+@Component({
+    // must configure the decorator which is stored as meta data for this class
+    // the selector is basically the HTML tag by which you use this component later
+    // should be a unique string, **DO NOT OVERRIDE AN EXISTING HTML ELEMENT**
+    selector: 'app-server',
+    // templateUrl is the html file associated with this component
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // this selector makes the component an HTML Tag like so <app-servers></app-servers>
+  selector: 'app-servers',
+  // this selector makes the component an HTML Attribute like so <div app-servers></div>
+  // selector: '[app-servers]',
+  // this selector makes this a HTML class like so <div class="app-servers"></div> **SELECTING BY ID DOES NOT WORK**
+  // selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

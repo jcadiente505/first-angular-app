e66238a282a3d4b176205c7d8fbaada6edaa4736
Cadman <jcadiente505@gmail.com>
2018-12-20 15:19:48 -0500

    Working on adding directives via attributes w/property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -13,7 +13,16 @@ export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = "offline";
     // Practicing method in components
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus() {
         return this.serverStatus
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,7 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus = "No server was created!";
   serverName="Test server";
-
+  serverCreated= false;
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true
@@ -25,8 +25,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
-
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true
   }
 
   onUpdateServerName(event: Event) {

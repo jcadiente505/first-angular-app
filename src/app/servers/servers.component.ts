0b4bae790aaf66f51e521f4906dc28fb841a6d4b
Cadman <jcadiente505@gmail.com>
2018-12-20 14:22:09 -0500

    event binding and string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false
-
+  serverCreationStatus = "No server was created!"
 
   constructor() { 
     setTimeout(() => {
@@ -23,4 +23,8 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+    
+  }
 }
